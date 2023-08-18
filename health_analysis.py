56882d3b9023ea0101c79d52da25ab70338f8d90
EugeneHsiung <141866888+EugeneHsiung@users.noreply.github.com>
2023-08-18 17:57:06 -0400

    health_analysis.py
    updated code

--- health_analysis.py
+++ health_analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 oxygen_saturation = int(input('put in your current oxygen_saturation: '))
+heart_rate= int(input('put in your heart rate: '))
 
 name = input('enter Your Name: ')
 
@@ -16,6 +17,12 @@ oxygen_saturation_levels = {
     'hyperxia': (100, 120),
     'normal': (95, 100),
     'hypoxia': (60, 94)}
+
+heart_rate_levels = {
+    'tachycardia': (100, 140),
+    'bradycardia': (1,60),
+    'normal': (60, 100)
+}
 
 #patient 1: John Smith, symptoms: dizziness, disorientation, visual disturbance
 #patient 2: Jason Smith, symptoms: headache, rapid heart rate, coughing
@@ -33,24 +40,34 @@ patient_data = {
 }
 
 
-def oxygensaturationchecker(oxygen_saturation): 
-    if oxygen_saturation >= 95 and oxygen_saturation <=100:
+def oxygensaturationchecker(oxygen_saturation, heart_rate): 
+    if (oxygen_saturation >= 95 and oxygen_saturation <=100) and (heart_rate >=60 and heart_rate <100):
         result = 'normal'
-    if oxygen_saturation < 95:
+    if oxygen_saturation < 95 and heart_rate <60: 
         result = 'below normal'
-    if oxygen_saturation > 100:
+    else:
         result = 'above normal'
     return result
 
 def oxygenationstatus(oxygen_saturation):
     if oxygen_saturation > 100 and oxygen_saturation <120:
-        show = 'hyperxia'
+        show = 'hyperxia' 
     if oxygen_saturation >=60 and oxygen_saturation <94:
         show= 'hypoxia'
     if oxygen_saturation >94 and oxygen_saturation <=100:    
         show= 'normal oxygenation'    
     return show
 
-#example: 
-print('hello,', name,'.' , 'Your result is', oxygensaturationchecker(oxygen_saturation), 'with a SPO2 of', oxygen_saturation, '%.', 'you have', oxygenationstatus (oxygen_saturation) )
+def heart_rate_status(heart_rate_levels):
+    if heart_rate_levels > 100: 
+        you_are = 'tachycardia' 
+    if heart_rate_levels <60:
+        you_are= 'bradycardia'
+    if heart_rate_levels >=60 and heart_rate_levels <=100: 
+        you_are= 'normal heart rate'    
+    return you_are 
 
+
+#example: 
+print('hello,', name,'.' , 'Your result is', oxygensaturationchecker(oxygen_saturation, heart_rate), 'with a SPO2 of', oxygen_saturation, '%.', 'you have', oxygenationstatus (oxygen_saturation) )
+print('your heart rate is', heart_rate, 'indicating', heart_rate_status (heart_rate))
